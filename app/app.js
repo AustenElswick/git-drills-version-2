b2a15a8f6df85123494d59fdae315137f4c376b6
Autumn Raine <autumn.raine@icloud.com>
2018-04-05 11:20:46 -0600

    added sum function

--- app/app.js
+++ app/app.js
@@ -8,3 +8,12 @@ const people = [
     age: 29
   }
 ]
+ const arrayOfNums = [2,4,6,8];
+
+ function sumOfNum(arr) {
+   let sum = 0;
+   for(let i = 0; i < arr.length; i++) {
+     sum += arr[i]
+   }
+   return sum;
+ }
